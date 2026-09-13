e28d753421136fef8dbd864e1bccadd80c321e20
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2026-09-13 18:27:45 +0400

    add sortable list example

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import DnDPlacement from "./examples/DnDPlacement";
 import DragOverlayExample from "./examples/DragOverlayExample";
+import SortableList from "./examples/SortableList";
 
 export default function App() {
-  return <DragOverlayExample />;
+  return <SortableList />;
 }
